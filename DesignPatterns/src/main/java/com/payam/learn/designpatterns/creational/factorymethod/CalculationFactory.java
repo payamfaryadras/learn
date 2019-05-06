1498b4691be31b38ce7311db176c1bfc23c021fa
payam <payam.ff@gmail.com>
2019-05-06 18:19:10 +0430

    refactoring
    change param name

--- DesignPatterns/src/main/java/com/payam/learn/designpatterns/creational/factorymethod/CalculationFactory.java
+++ DesignPatterns/src/main/java/com/payam/learn/designpatterns/creational/factorymethod/CalculationFactory.java
@@ -2,9 +2,9 @@ package com.payam.learn.designpatterns.creational.factorymethod;
 
 public class CalculationFactory {
 
-    public static Calculation createCalculator(long amountPerMonth, long taxPercent, String product, boolean ferstival) {
+    public static Calculation createCalculator(long amountPerMonth, long taxPercent, String product, boolean isCalculation1) {
 
-        if (ferstival) {
+        if (isCalculation1) {
             return new Calculation1(amountPerMonth, taxPercent, product);
         }
         return new Calculation2(amountPerMonth, taxPercent, product);
