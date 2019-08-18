806fd137d5a30a6bb0c65b0d8815486e04307c20
Ehsan Yousefi <ehsan.fortuna@gmail.com>
2019-08-18 15:09:46 +0430

    change file

--- DesignPatterns/src/main/java/com/payam/learn/designpatterns/structural/facade/DiscountPremiumCalculationService.java
+++ DesignPatterns/src/main/java/com/payam/learn/designpatterns/structural/facade/DiscountPremiumCalculationService.java
@@ -1,5 +1,9 @@
 package com.payam.learn.designpatterns.structural.facade;
 
+
+//// this file changed by ehsan 
+
+
 public class DiscountPremiumCalculationService {
     public double calculation(Object object){
         return 0d;
